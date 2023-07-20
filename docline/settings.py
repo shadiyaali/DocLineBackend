404eb194c99bce78e64f73b9c259cfa3ae572f41
Shadiya <shadiyackn@gmail.com>
2023-07-20 11:03:22 +0530

    newss

--- docline/settings.py
+++ docline/settings.py
@@ -225,6 +225,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://doc-line-frondent.vercel.app",
  ]
 CORS_ALLOW_ALL_HEADERS = True
 CORS_ALLOW_ALL_METHODS = True
