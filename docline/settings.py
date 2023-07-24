cf4044227b22ae21e7acd53ed65cd6015b950019
Shadiya <shadiyackn@gmail.com>
2023-07-24 12:17:02 +0530

    newss

--- docline/settings.py
+++ docline/settings.py
@@ -172,20 +172,20 @@ CHANNEL_LAYERS = {
 #     }
 # }
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#         "TEST": {
-#             "NAME": BASE_DIR / "db.sqlite3",
-#         },
-#     }
-# }
-
-import dj_database_url
 DATABASES = {
-    "default":dj_database_url.parse(config('DATABASE_URL'))
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        "TEST": {
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
+    }
 }
+
+# import dj_database_url
+# DATABASES = {
+#     "default":dj_database_url.parse(config('DATABASES_URL'))
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
