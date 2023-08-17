7e31851f8e9e4217c8dfba83b965bb1b351db142
Shadiya <shadiyackn@gmail.com>
2023-08-17 09:51:32 +0530

    render postgres

--- docline/settings.py
+++ docline/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from datetime import timedelta
 from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -172,21 +173,22 @@ CHANNEL_LAYERS = {
 #     }
 # }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#         "TEST": {
+#             "NAME": BASE_DIR / "db.sqlite3",
+#         },
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-        "TEST": {
-            "NAME": BASE_DIR / "db.sqlite3",
-        },
-    }
+    "default":dj_database_url.parse(config('DATABASES_URL'))
 }
 
-# import dj_database_url
-# DATABASES = {
-#     "default":dj_database_url.parse(config('DATABASES_URL'))
-# }
-# Password validation
+# # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
