f66be32c06a35d8c0dce18af5bd08717656ea29c
Shadiya <shadiyackn@gmail.com>
2023-12-11 19:16:38 +0530

    image

--- docline/settings.py
+++ docline/settings.py
@@ -230,7 +230,7 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'user.User' 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
